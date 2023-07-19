4f4efe8779a1fe105661eeaaf9cf85192d4795ab
Amit258012 <amit258012@gmail.com>
2023-07-19 12:37:23 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Button } from "./components/ui/button";
+
+function App() {
+	return (
+		<>
+			<h1>Hello</h1>
+			<Button>Click me</Button>
+		</>
+	);
+}
+
+export default App;
